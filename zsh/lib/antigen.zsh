cd125d1e6d038b796e47474e39fab74d4243063e
Daniel Goeke <dgo@stderror.net>
2019-01-26 23:05:42 +0100

    add zsh stuff

---
+++ zsh/lib/antigen.zsh
@@ -0,0 +1,26 @@
+# set -x
+
+############################################################
+# Antigen
+source ${HOME}/.config/zsh/antigen.zsh
+
+antigen use oh-my-zsh
+
+# load bundles
+antigen bundle dotenv
+antigen bundle gradle
+antigen bundle docker
+antigen bundle golang
+antigen bundle history
+antigen bundle sudo
+antigen bundle alexrochas/zsh-extract
+antigen bundle alexrochas/zsh-vim-crtl-z
+antigen bundle zsh-users/zsh-completions
+antigen bundle zdharma/fast-syntax-highlighting
+antigen bundle Tarrasch/zsh-autoenv
+antigen bundle mafredri/zsh-async
+antigen bundle sindresorhus/pure
+antigen bundle laurenkt/zsh-vimto
+
+# apply
+antigen apply

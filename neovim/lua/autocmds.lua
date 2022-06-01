4f684f7068e2b8994d66d00d922dfb43a09d0dc8
Daniel Goeke <daniel.goeke@jodel.com>
2022-06-01 11:14:07 +0200

    rewrite autocmd to remove trailing spaces to exclude markdown
    * write it in pure lua

--- neovim/lua/autocmds.lua
+++ neovim/lua/autocmds.lua
@@ -1,11 +1,30 @@
 -------------------------------------------------------------------------------
 -- Remove all trailing whitespace on save
-vim.api.nvim_exec([[
-  augroup TrimWhiteSpace
-    au!
-    autocmd BufWritePre * :%s/\s\+$//e
-  augroup END
-]], false)
+
+-- check if current filetype is in filetypes array
+local match_filetype = function(filetypes)
+  for _, filetype in ipairs(filetypes) do
+    if vim.bo.filetype == filetype then
+      return true
+    end
+  end
+  return false
+end
+
+vim.api.nvim_create_augroup("TrimWhiteSpace", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+    callback = function()
+      disabled_filetypes = {"markdown"}
+      if not match_filetype(disabled_filetypes) then
+        local save = vim.fn.winsaveview()
+        vim.api.nvim_exec([[keepjumps keeppatterns silent! %s/\s\+$//e]], false)          -- removes all trailing whitspace
+        vim.api.nvim_exec([[keepjumps keeppatterns silent! %s/\($\n\s*\)\+\%$//]], false) -- trim last line
+        vim.fn.winrestview(save)
+      end
+    end,
+    desc = "CleanupFile",
+    group = "TrimWhiteSpace"
+})
 
 -------------------------------------------------------------------------------
 -- Prevent new line to also start with a comment

f74d9386caf594da91f77874ed0c032bc23b0394
Daniel Goeke <daniel.goeke@jodel.com>
2022-10-06 09:50:16 +0200

    neovi: update tree settings

--- neovim/lua/plugins/configs/tree.lua
+++ neovim/lua/plugins/configs/tree.lua
@@ -8,10 +8,6 @@ require'nvim-tree'.setup {
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = false,
     icons = {
@@ -44,7 +40,6 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = false,
     mappings = {
       custom_only = false,
       list = {}

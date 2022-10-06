88444c0a8c4f5cc584fd19320f40f7c3cad4418d
Daniel Goeke <daniel.goeke@jodel.com>
2022-10-06 16:32:44 +0200

    zsh: improve path adding

--- zsh/lib/00-functions.zsh
+++ zsh/lib/00-functions.zsh
@@ -1,18 +1,24 @@
 # contains base function used in my zsh setup
 
 # add $1 in front of that path if it is not already in
-pathadd() {
-  new="${1}"
+pathadd_pre() {
+  new="${1:A}"
   if [ -d "${new}" ]; then
     if [[ ":${PATH}:" != *":${new}:"* ]]; then
       PATH="${new}:${PATH}"
-    fi  
-  fi  
+    fi
+  fi
+}
+
+pathadd_post(){
+  new="${1:A}"
+  if [ -d "${new}" ]; then
+    if [[ ":${PATH}:" != *":${new}:"* ]]; then
+      PATH="${PATH}:${new}"
+    fi
+  fi
 }
 
 
 ## check if given command exists on the system
-command_exists() {
-  command -v "${1}" > /dev/null
-  return $?
-}
+command_exists() { (( $+commands[$1] )) }

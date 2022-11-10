a7884e1d57d1482de047a05c31632a117c05d3cf
Daniel Goeke <dgo@stderror.net>
2022-11-10 14:31:54 +0100

    add ssh config
    clean nvim packer_compiled

--- dot_config/nvim/lua/plugins/configs/lspconfig.lua
+++ dot_config/nvim/lua/plugins/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 local lspconfig = require("lspconfig")
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- golang
 lspconfig.gopls.setup {

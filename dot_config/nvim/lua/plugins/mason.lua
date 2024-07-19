5476e8789318ced60bacc03c35fc5360038f580e
Daniel Goeke <dgo@stderror.net>
2024-07-19 14:30:28 +0200

    Neovim: Added lazyvim configuration

---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "ansible-language-server",
+        "ansible-lint",
+        "bash-language-server",
+        "dockerfile-language-server",
+        "editorconfig-checker",
+        "golangci-lint",
+        "gopls",
+        "shellcheck",
+        "shfmt",
+        "stylua",
+        "templ",
+        "terraform-ls",
+        "zls",
+      },
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "css",
+        "csv",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "gomod",
+        "gosum",
+        "gotmpl",
+        "html",
+        "json",
+        "kconfig",
+        "lua",
+        "sql",
+        "ssh_config",
+        "terraform",
+        "xml",
+        "yaml",
+        "zig",
+      },
+    },
+  },
+}

bb53f677c455bfd05851d73b9150a1264f4c648d
Daniel Goeke <dgo@stderror.net>
2024-11-01 20:29:01 +0100

    vim: remove old files
    Theses files are no longer needed.

--- dot_config/nvim/lua/mappings.lua
+++
@@ -1,8 +0,0 @@
-local keymap = vim.api.nvim_set_keymap
-default_options = {noremap = true, silent = true}
-expr_options = {noremap = true, expr = true, silent = true}
-
--- map the leader key
-keymap("n", "<Space>", "<NOP>", default_options)
-vim.g.mapleader = " "
-
